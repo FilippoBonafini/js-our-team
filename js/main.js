fbc9ea7f9176d8bac51ea8dbfd0f6249922446a5
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-07 15:21:45 +0100

    MILESTONE 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const member = [
+    {
+        nome: 'Pippo Bona',
+        ruolo: 'Manager',
+        foto: 'img/img1.png'
+    },
+    {
+        nome: 'Pippo Bona',
+        ruolo: 'Manager',
+        foto: 'img/img1.png'
+    },
+    {
+        nome: 'Pippo Bona',
+        ruolo: 'Manager',
+        foto: 'img/img1.png'
+    },
+    {
+        nome: 'Pippo Bona',
+        ruolo: 'Manager',
+        foto: 'img/img1.png'
+    },
+    {
+        nome: 'Pippo Bona',
+        ruolo: 'Manager',
+        foto: 'img/img1.png'
+    },
+    {
+        nome: 'Pippo Bona',
+        ruolo: 'Manager',
+        foto: 'img/img1.png'
+    }
+]
+
+for (let i = 0; i < member.length; i++) {
+    const oggetto = member[i];
+    console.log('ECCO LE INFORMAZIONI DI: ' + oggetto.nome);
+    for (let key in oggetto) {
+        console.log(oggetto[key]);
+    }
+    console.log('');
+}
